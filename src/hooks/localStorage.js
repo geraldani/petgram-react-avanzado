3a9345b99b11ed5375ba669e7e015dc0cdd08ba1
Geraldyn Chirinos <geraldynchidesousa@gmail.com>
2019-10-14 23:04:30 -0300

    importando librerias

--- src/hooks/localStorage.js
+++ src/hooks/localStorage.js
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 
 export function useLocalStorage (initialValue, key) {
+  /* Las claves y los valores de localStorage, son siempre cadenas de texto
+  (tener en cuenta que, al igual que con los objetos, las claves de enteros se convertirán automáticamente en cadenas de texto). */
   const [storeValue, setStoreValue] = useState(() => {
     try {
       const elem = window.localStorage.getItem(key)
-      return elem === null ? initialValue : JSON.parse(elem)
+      return elem === null ? initialValue : JSON.parse(elem) // JSON.parse convierte una cadena json a el objeto q lo representa
     } catch (e) {
       return initialValue
     }
@@ -12,7 +14,7 @@ export function useLocalStorage (initialValue, key) {
 
   const setLocalStorage = value => {
     try {
-      window.localStorage.setItem(key, JSON.stringify(value))
+      window.localStorage.setItem(key, JSON.stringify(value)) // JSON.stringify convierte un objeto a una cadena json objeto
       setStoreValue(value)
     } catch (err) {
       console.log('Error: ', err.message)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 function render () {
   ReactDOM.render(<App />, document.getElementById('app'))
 }
+
 render()
 
 if (module.hot) {
